4989002f5171f8a58bfe76b7fb72c57efb276227
vand-dev <vandersonaugusto6@gmail.com>
2023-11-07 11:51:47 -0300

    Using product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,4 @@
-const produts = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      starts: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      starts: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      starts: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
+
 //variavel que vai armazenar cada produto novo 
 let productsHTML= '' ;
 
@@ -44,14 +16,14 @@ produts.forEach((produts) => {
 
             <div class="product-rating-container">
                 <img class="product-rating-stars"
-                src="images/ratings/rating-${produts.rating.starts * 10}.png">
+                src="images/ratings/rating-${produts.rating.stars * 10}.png">
                 <div class="product-rating-count link-primary">
                 ${produts.rating.count}
                 </div>
             </div>
 
-            <div class="product-price"> //.tofixed deixa 2 casas decimais
-                ${(produts.priceCents / 100).toFixed(2)} 
+            <div class="product-price"> <!-- tofixed deixa 2 casas decimais -->
+                $${(produts.priceCents / 100).toFixed(2)} 
             </div>
 
             <div class="product-quantity-container">
