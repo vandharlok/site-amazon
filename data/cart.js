d1d36a7462f5c9a0a6dc70eca425eecd2b165fa4
vand-dev <vandersonaugusto6@gmail.com>
2023-11-17 15:28:26 -0300

    save and generate html for delivery options

--- data/cart.js
+++ data/cart.js
@@ -2,25 +2,19 @@
 //
 
 export let cart = JSON.parse(localStorage.getItem('cart'));
-/*
 
-[
-  {
+if(!cart){
+  cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 1,
-  },
-  {
-    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-  },
-  {
-    productId:'83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-    quantity: 1,
-  }
-  
- 
-];
-*/
+    quantity:1,
+    deliveryOptionId:'1'
+
+  },{
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity:2,
+    deliveryOptionId:'2'
+  }];
+}
 //the localstore just save strings, so, i ve to convert the cart in string,
 // receive two parameters, the name of i wanna save, and the data, both of them in strings 
 function saveLocalStore(){
@@ -37,6 +31,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId: '1',
     });
     saveLocalStore();
   }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,12 +1,21 @@
 import {cart} from '../data/cart.js';
 import {products} from '../data/products.js';
-import { formatCurrency } from './utils/money.js'
+import { formatCurrency } from './utils/money.js';
 import { removeFromCart } from '../data/cart.js';
-
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import {deliveryOptions} from '../data/deliveryOptions.js'
 
 //variavel que vai receber o html para toda vez atualizar meu html com os novos itens
 // do carrinho
 let cartSummaryHTML=""
+
+const today=dayjs();
+//the method add in dayjs adding a time in ur current time, receiving two
+//parameters, how much, and if its day or minutes etc
+
+const deliveryDate= today.add(7,'days');
+//console.log(deliveryDate.format('dddd MMMM D'));
+
 
 
 cart.forEach((cartItem) => {
@@ -26,11 +35,31 @@ cart.forEach((cartItem) => {
   
     }) // a parte do input selector dando o valor do id, faz com que eu possa selecionar os outros seletores, ja que eu posso slecionar apenas um com o mesmo nome, e quando passo o 
     // id, posso ser unico para cada name.
+//mesma coisa aki
+
+let deliveryOptionId= cartItem.deliveryOptionId;
+//console.log(deliveryOptionId);
+let deliveryOption;
+
+deliveryOptions.forEach((option) => {
+    if(option.id === deliveryOptionId){
+        deliveryOption=option;
+        
+       
+    }
+});
+
+const today=dayjs();
+const deliveryDate=today.add(deliveryOption.deliveryDays,'days');
+const dateString= deliveryDate.format('dddd MMMM D');
+
+
+
     cartSummaryHTML += 
 `   <div class="cart-item-container 
     js-cart-item-container-${matchingProduct.id}"> 
         <div class="delivery-date">
-            Delivery date: Tuesday, June 21
+            Delivery date: ${dateString}
         </div>
 
         <div class="cart-item-details-grid">
@@ -61,46 +90,9 @@ cart.forEach((cartItem) => {
             <div class="delivery-options-title">
                 Choose a delivery option:
             </div>
-            <div class="delivery-option">
-                <input type="radio" checked
-                class="delivery-option-input"
-                
-                name="delivery-option-${matchingProduct.id}">
-                <div>
-                <div class="delivery-option-date">
-                    Tuesday, June 21
-                </div>
-                <div class="delivery-option-price">
-                    FREE Shipping
-                </div>
-                </div>
-            </div>
-            <div class="delivery-option">
-                <input type="radio"
-                class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-                <div>
-                <div class="delivery-option-date">
-                    Wednesday, June 15
-                </div>
-                <div class="delivery-option-price">
-                    $4.99 - Shipping
-                </div>
-                </div>
-            </div>
-            <div class="delivery-option">
-                <input type="radio"
-                class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-                <div>
-                <div class="delivery-option-date">
-                    Monday, June 13
-                </div>
-                <div class="delivery-option-price">
-                    $9.99 - Shipping
-                </div>
-                </div>
-            </div>
+            ${deliveryOptionsHTML(matchingProduct,cartItem)}
+            
+            
             </div>
         </div>
         </div>
@@ -108,10 +100,54 @@ cart.forEach((cartItem) => {
 
 
 
-`;
+`
 
 
 })
+//Loop through deliveryOptions, for each option, generate some html, and combine them
+function deliveryOptionsHTML(matchingProduct,cartItem) {
+
+    let html= ``;
+    
+
+
+    deliveryOptions.forEach((deliveryOption) => {
+         
+        const today=dayjs();
+        const deliveryDate=today.add(deliveryOption.deliveryDays,'days');
+        
+        const dateString= deliveryDate.format('dddd MMMM D');
+        const priceString = deliveryOption.priceCents 
+        ===0
+        ? 'FREE'
+        : `${formatCurrency(deliveryOption.priceCents)} -`;
+
+
+        let isChecked= deliveryOption.id===cartItem.deliveryOptionId;
+        
+        
+        html +=
+
+        `<div class="delivery-option">
+        <input type="radio"
+        ${isChecked ? 'checked' :"" }
+        class="delivery-option-input"
+        name="delivery-option-${matchingProduct.id}">
+        <div>
+        <div class="delivery-option-date">
+            ${dateString};
+        </div>
+        <div class="delivery-option-price">
+            ${priceString} - Shipping
+        </div>
+        </div>
+    </div>`
+    });
+    return html;
+
+
+
+}
 //gererating the html automatically
 
 document.addEventListener("DOMContentLoaded", (event) => {
